eb57004cc5f42890d4f482cbcf938351990a4cd9
tarunkethwalia <tarunkethwalia18@gmail.com>
2021-10-20 15:04:35 +0530

    email-api

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+server.listen(PORT,'192.168.21.248', () => {
     console.log('Server is running on port: '+PORT);
 });
